4311fa2e7c6c149ae0a7b57a0b70d3211e4aa281
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-02-29 14:53:48 -0300

    feat: FinançasApp configuração de rotas

---
+++ FinancasApp/src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>TELA LOGIN</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
