5ce28324ad19c9ca03ee219447b05083d6a78099
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-03-07 13:46:35 -0300

    feat: página e funcionalidade de SignUp

---
+++ FinancasApp/src/contexts/auth.js
@@ -0,0 +1,34 @@
+import React, {createContext, useState} from 'react';
+import api from './../services/api';
+import {useNavigation} from '@react-navigation/native';
+export const AuthContext = createContext({});
+
+function AuthProvider({children}) {
+  const [user, setUser] = useState({
+    nome: 'Caroline Teste',
+  });
+
+  const navigation = useNavigation();
+
+  async function signUp(name, password, email) {
+    try {
+      const response = await api.post('/users', {
+        name: name,
+        password: password,
+        email: email,
+      });
+
+      navigation.goBack();
+    } catch (error) {
+      console.log('Erro ao cadastrar', error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{user, signUp}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
--- FinancasApp/src/pages/SignUp/index.js
+++ FinancasApp/src/pages/SignUp/index.js
@@ -1,12 +1,56 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {Platform} from 'react-native';
+import {
+  Background,
+  Container,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+} from '../SignIn/styles.js';
+
+import {AuthContext} from './../../contexts/auth';
 
 export default function SignUp() {
+  const {signUp} = useContext(AuthContext);
+
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  function handleSignUp() {
+    signUp(name, password, email);
+  }
+
   return (
-    <View>
-      <Text>TELA DE CADASTRO</Text>
-    </View>
+    <Background>
+      <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled>
+        <AreaInput>
+          <Input
+            placeholder="Seu nome"
+            value={name}
+            onChangeText={text => setName(text)}
+          />
+        </AreaInput>
+        <AreaInput>
+          <Input
+            placeholder="Email"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+        </AreaInput>
+        <AreaInput>
+          <Input
+            placeholder="Senha"
+            value={password}
+            onChangeText={text => setPassword(text)}
+            secureTextEntry={true}
+          />
+        </AreaInput>
+        <SubmitButton activeOpacity={0.8} onPress={handleSignUp}>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+      </Container>
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({});
